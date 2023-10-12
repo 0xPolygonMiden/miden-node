576de2c2f24f8b8cc2c001de4dbda6448c6e8639
Philippe Laferriere <plafer@protonmail.com>
2023-10-12 16:38:23 -0400

    add request_partial_merkle_trees

--- txpool/src/tasks/block_producer/mod.rs
+++ txpool/src/tasks/block_producer/mod.rs
@@ -1,7 +1,7 @@
 use async_trait::async_trait;
 use core::fmt::Debug;
 use core::time::Duration;
-use miden_objects::BlockHeader;
+use miden_objects::{accounts::AccountId, crypto::merkle::PartialMerkleTree, BlockHeader, Digest};
 use tokio::time;
 
 use crate::{Block, BlockData, TxBatch};
@@ -12,10 +12,18 @@ pub enum Error {}
 #[async_trait]
 pub trait BlockProducerTaskHandle {
     type RecvError: Debug;
+    type MerkleTreesReqError: Debug;
     type ApplyBlockError: Debug;
 
     /// Receive a new transaction batch
     async fn receive_tx_batch(&self) -> Result<TxBatch, Self::RecvError>;
+
+    /// Requests the partial merkle trees associated with the updated accounts, notes and nullifiers.
+    /// TODO: also request for notes and nullifiers
+    async fn request_partial_merkle_trees(
+        &self,
+        updated_account_ids: Vec<AccountId>,
+    ) -> Result<PartialMerkleTree, Self::MerkleTreesReqError>;
 
     /// Output the produced block
     async fn apply_block(
@@ -79,15 +87,41 @@ impl<H: BlockProducerTaskHandle> BlockProducerTask<H> {
         &self,
         tx_batch: TxBatch,
     ) -> Result<Block, Error> {
-        let updated_account_state_hashes: Vec<_> = tx_batch
-            .into_iter()
-            .map(|tx| (tx.account_id(), tx.final_account_hash()))
-            .collect();
+        let parsed_tx_batch = ParsedTxBatch::from(tx_batch);
+
+        let account_pmt = self
+            .handle
+            .request_partial_merkle_trees(parsed_tx_batch.updated_account_ids.clone())
+            .await
+            .expect("Request to get the partial merkle trees failed");
+
+        // TODO: compute new account db root from pmt
 
         let _block_body = BlockData {
-            updated_account_state_hashes,
+            updated_account_state_hashes: parsed_tx_batch.updated_account_state_hashes,
         };
 
         todo!()
+    }
+}
+
+#[derive(Default)]
+struct ParsedTxBatch {
+    updated_account_ids: Vec<AccountId>,
+    updated_account_state_hashes: Vec<(AccountId, Digest)>,
+}
+
+impl From<TxBatch> for ParsedTxBatch {
+    fn from(tx_batch: TxBatch) -> Self {
+        let mut parsed_tx_batch = ParsedTxBatch::default();
+
+        for tx in tx_batch {
+            parsed_tx_batch.updated_account_ids.push(tx.account_id());
+            parsed_tx_batch
+                .updated_account_state_hashes
+                .push((tx.account_id(), tx.final_account_hash()))
+        }
+
+        parsed_tx_batch
     }
 }
