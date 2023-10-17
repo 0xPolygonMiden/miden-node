2d8dbb7cdcd8da2602f721a2f49d2e58d9e9514f
Philippe Laferriere <plafer@protonmail.com>
2023-10-17 15:41:25 -0400

    test_tx_timer_resets_after_full_batch_sent

--- tx_pipeline/src/tasks/txqueue/tests/batch_sent.rs
+++ tx_pipeline/src/tasks/txqueue/tests/batch_sent.rs
@@ -120,3 +120,66 @@ async fn test_timer_send_batch() {
     // Ensure that the queue is empty
     assert!(tx_queue.ready_queue.lock().await.is_empty());
 }
+
+/// This tests if the internal transaction timer properly resets after a full
+/// batch is sent.
+///
+/// We set the batch size to 2, send 3 transactions (see diagram), and confirm
+/// that 2 were properly sent initially. The tx timer is set to 40ms. After
+/// 55ms, we confirm that we have only 1 batch sent (it would be 2 if the first
+/// timer was not reset). After 80ms, ensure that both batches were sent.
+///
+///        timer                             confirm sent         confirm sent
+///        reset                             1 batch               2 batches
+///  tx1    tx2             tx3             
+///  |       |       |       |       |       |       |       |       |
+///  0ms    10ms    20ms    30ms    40ms   50ms    60ms    70ms     80ms
+///  |-------timer (cancelled)-------|
+///                          |--------------timer------------|
+///
+/// Specifically, we're ensuring that tx3 is not sent early (at 50ms), which
+/// would happen if the initial timer was not properly cancelled
+#[tokio::test]
+async fn test_tx_timer_resets_after_full_batch_sent() {
+    let batch_size: usize = 2;
+
+    let handle_in = HandleInVariableInterval::new(vec![
+        Duration::from_millis(0),
+        Duration::from_millis(10),
+        Duration::from_millis(20),
+    ]);
+    let handle_out = HandleOutDefault::new();
+
+    let tx_queue = TxQueue::new(
+        handle_in,
+        handle_out,
+        TxQueueOptions {
+            batch_size,
+            tx_max_time_in_queue: Duration::from_millis(40),
+        },
+    );
+
+    // Spawn tx_queue task
+    {
+        let tx_queue = tx_queue.clone();
+        tokio::spawn(tx_queue.run());
+    }
+
+    time::sleep(Duration::from_millis(55)).await;
+
+    // Ensure that 1 batch was sent
+    assert_eq!(tx_queue.handle_out.batches.read().await.len(), 1);
+    // Ensure that the queue holds 1 transaction
+    assert_eq!(tx_queue.ready_queue.lock().await.len(), 1);
+
+    time::sleep(Duration::from_millis(35)).await;
+
+    // Ensure that 2 batches were sent
+    assert_eq!(tx_queue.handle_out.batches.read().await.len(), 2);
+    // Ensure that first batch received is length 2
+    assert_eq!(tx_queue.handle_out.batches.read().await[0].len(), 2);
+    // Ensure that second batch received is length 1
+    assert_eq!(tx_queue.handle_out.batches.read().await[1].len(), 1);
+    // Ensure that the queue is empty
+    assert!(tx_queue.ready_queue.lock().await.is_empty());
+}
