3c8ef01e72cd0f87f7964871a8c95207c7edc821
Philippe Laferriere <plafer@protonmail.com>
2023-10-18 17:30:25 -0400

    ReadTxClientVariableInterval

--- block-producer/src/tasks/txqueuerpc/tests/mod.rs
+++ block-producer/src/tasks/txqueuerpc/tests/mod.rs
@@ -38,3 +38,36 @@ impl ReadTxClientFixedInterval {
         }
     }
 }
+
+pub struct ReadTxClientVariableInterval {
+    read_tx_client: RpcClient<ProvenTransaction, ()>,
+    /// Encodes how long to wait before sending the ith transaction. Thus, we send
+    /// `interval_durations.len()` transactions.
+    interval_durations: Vec<Duration>,
+    proven_tx_gen: DummyProvenTxGenerator,
+}
+
+impl ReadTxClientVariableInterval {
+    pub fn new(
+        read_tx_client: RpcClient<ProvenTransaction, ()>,
+        interval_durations: Vec<Duration>,
+    ) -> Self {
+        Self {
+            read_tx_client,
+            interval_durations,
+            proven_tx_gen: DummyProvenTxGenerator::new(),
+        }
+    }
+
+    pub async fn run(self) {
+        for duration in self.interval_durations {
+            time::sleep(duration).await;
+
+            self.read_tx_client
+                .call(self.proven_tx_gen.dummy_proven_tx())
+                .unwrap()
+                .await
+                .unwrap();
+        }
+    }
+}
