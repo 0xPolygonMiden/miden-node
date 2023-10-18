55c64a84e9514d09e9a7c536b891159bef1b0a8c
Philippe Laferriere <plafer@protonmail.com>
2023-10-18 13:25:50 -0400

    rpc init

--- block-producer/src/lib.rs
+++ block-producer/src/lib.rs
@@ -5,6 +5,8 @@ use miden_objects::transaction::ProvenTransaction;
 #[cfg(test)]
 pub mod test_utils;
 
+// TODO: Better name than rpc?
+pub mod rpc;
 pub mod tasks;
 
 type SharedProvenTx = Arc<ProvenTransaction>;
---
+++ block-producer/src/rpc/mod.rs
@@ -0,0 +1,37 @@
+mod local;
+
+use async_trait::async_trait;
+
+/// Creates a client/server pair that communicate locally using tokio channels
+pub use local::create_local_client_server_pair;
+
+/// Errors related to the RPC mechanism itself
+/// TODO: Make errors more descriptive
+#[derive(Debug)]
+pub enum RpcError {
+    SendError,
+    RecvError,
+}
+
+#[async_trait]
+pub trait Rpc<Request, Response>: Send + Sync
+where
+    Request: Send + 'static,
+    Response: Send + 'static,
+{
+    async fn handle_request(&self, x: Request) -> Response;
+}
+
+#[async_trait]
+pub trait RpcServer {
+    async fn serve(mut self) -> Result<(), RpcError>;
+}
+
+#[async_trait]
+pub trait RpcClient<Request, Response>
+where
+    Request: Send + 'static,
+    Response: Send + 'static,
+{
+    async fn call(&mut self, x: Request) -> Result<Response, RpcError>;
+}
