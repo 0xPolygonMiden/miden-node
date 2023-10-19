1bc4d21c9bd8ed5cd6139f8d1db82ef919161b9e
Philippe Laferriere <plafer@protonmail.com>
2023-10-19 15:40:07 -0400

    implement get_batches() handler

--- block-producer/src/tasks/batch_builder/mod.rs
+++ block-producer/src/tasks/batch_builder/mod.rs
@@ -4,6 +4,8 @@ use async_trait::async_trait;
 
 use crate::{msg::MessageHandler, SharedMutVec, SharedProvenTx};
 
+type ReadyBatchQueue = SharedMutVec<TxBatch>;
+
 /// A batch of transactions that have been proven with a single recursive proof.
 ///
 /// FIXME: Properly define this type. For now, we store the proven transactions that go in the batch
@@ -11,7 +13,12 @@ pub struct TxBatch {
     proven_txs: Vec<SharedProvenTx>,
 }
 
-type ReadyBatchQueue = SharedMutVec<TxBatch>;
+impl TxBatch {
+    /// Returns the number of transactions in the batch
+    pub fn num_txs(&self) -> usize {
+        self.proven_txs.len()
+    }
+}
 
 // Batch Builder
 // ================================================================================================
@@ -25,7 +32,6 @@ struct BatchBuilder {
 
 #[async_trait]
 impl MessageHandler<Vec<SharedProvenTx>, ()> for BatchBuilder {
-
     /// Handler for transaction queue's `send_txs()` message
     async fn handle_message(
         self: Arc<Self>,
@@ -33,9 +39,7 @@ impl MessageHandler<Vec<SharedProvenTx>, ()> for BatchBuilder {
     ) {
         // Note: Normally, we would actually process the message to create the `TxBatch`.
         // We need to properly define the `TxBatch` type first
-        let batch = TxBatch {
-            proven_txs: proven_txs,
-        };
+        let batch = TxBatch { proven_txs };
 
         self.ready_batches.lock().await.push(batch);
     }
@@ -43,15 +47,29 @@ impl MessageHandler<Vec<SharedProvenTx>, ()> for BatchBuilder {
 
 #[async_trait]
 impl MessageHandler<usize, Vec<TxBatch>> for BatchBuilder {
-
     /// Handler for block producer's `get_batches(max_num_txs)` message.
-    /// 
+    ///
     /// `max_num_txs` is the maximum number of transactions that must be contained in the sum of all
     /// batches.
     async fn handle_message(
         self: Arc<Self>,
         max_num_txs: usize,
     ) -> Vec<TxBatch> {
-        todo!()
+        let mut locked_ready_batches = self.ready_batches.lock().await;
+
+        let mut current_tx_count: usize = 0;
+        let mut num_batches_to_send = 0;
+
+        for batch in locked_ready_batches.iter() {
+            if current_tx_count + batch.num_txs() < max_num_txs {
+                num_batches_to_send += 1;
+            } else {
+                break;
+            }
+
+            current_tx_count += batch.num_txs();
+        }
+
+        locked_ready_batches.drain(..num_batches_to_send).collect()
     }
 }
