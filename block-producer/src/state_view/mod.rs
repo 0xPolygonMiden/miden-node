ed3960d7937c12c8b9c1b7a16ac75be32716ebf6
Philippe Laferriere <plafer@protonmail.com>
2023-10-25 12:53:08 -0400

    DefaultStateView fields

--- block-producer/src/state_view/mod.rs
+++ block-producer/src/state_view/mod.rs
@@ -1,6 +1,11 @@
-use std::sync::Arc;
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    sync::Arc,
+};
 
 use async_trait::async_trait;
+use miden_objects::{accounts::AccountId, Digest};
+use tokio::sync::RwLock;
 
 use crate::{
     block::Block,
@@ -25,6 +30,26 @@ where
     TI: GetTxInputs,
 {
     get_tx_inputs: Arc<TI>,
+
+    /// The account ID of accounts being modified by transactions currently in the block production
+    /// pipeline. We currently ensure that only 1 tx/block modifies any given account.
+    accounts_in_flight: Arc<RwLock<BTreeSet<AccountId>>>,
+
+    /// The nullifiers of notes consumed by transactions currently in the block production pipeline.
+    nullifiers_in_flight: Arc<RwLock<BTreeSet<Digest>>>,
+}
+
+impl<TI> DefaulStateView<TI>
+where
+    TI: GetTxInputs,
+{
+    pub fn new(get_tx_inputs: Arc<TI>) -> Self {
+        Self {
+            get_tx_inputs,
+            accounts_in_flight: Arc::new(RwLock::new(BTreeSet::new())),
+            nullifiers_in_flight: Arc::new(RwLock::new(BTreeSet::new())),
+        }
+    }
 }
 
 #[async_trait]
