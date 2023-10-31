5ac3971d39e8235b42303b637b89011ffae3cd47
Philippe Laferriere <plafer@protonmail.com>
2023-10-31 13:20:52 -0400

    nullifier scaffold

--- block-producer/src/block_builder/vm.rs
+++ block-producer/src/block_builder/vm.rs
@@ -1,21 +1,20 @@
 use miden_objects::{assembly::Assembler, Digest};
 use miden_stdlib::StdLibrary;
 
-const ACCOUNT_PROGRAM_SOURCE: &'static str = "
-
+const NULLIFIER_PROGRAM_SOURCE: &'static str = "
     begin
 
     end
 ";
 
-pub fn compute_new_account_root() -> Digest {
-    let account_program = {
+pub fn compute_new_nullifier_root() -> Digest {
+    let nullifier_program = {
         let assembler = Assembler::default()
             .with_library(&StdLibrary::default())
             .expect("failed to load std-lib");
 
         assembler
-            .compile(ACCOUNT_PROGRAM_SOURCE)
+            .compile(NULLIFIER_PROGRAM_SOURCE)
             .expect("failed to load account update program")
     };
 
