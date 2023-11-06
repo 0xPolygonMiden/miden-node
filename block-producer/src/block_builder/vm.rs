3a6ed99a47a7c5dd9e260a627f4c7687b1e667fd
Philippe Laferriere <plafer@protonmail.com>
2023-11-06 15:40:53 -0500

    write account root update masm

--- block-producer/src/block_builder/vm.rs
+++ block-producer/src/block_builder/vm.rs
@@ -6,15 +6,42 @@ use miden_objects::{
 use miden_stdlib::StdLibrary;
 use miden_vm::{AdviceInputs, DefaultHost, MemAdviceProvider};
 
-const ACCOUNT_PROGRAM_SOURCE: &'static str = "
-    begin
+/// Stack inputs: 
+/// [num_accounts_updated, 
+///  ACCOUNT_ROOT,
+///  NEW_ACCOUNT_HASH_0, account_id_0, ... , NEW_ACCOUNT_HASH_n, account_id_n]
+const ACCOUNT_UPDATE_ROOT_MASM: &'static str = "
+use.std::collections::smt64
 
+begin
+    push.1
+    while.true
+        # stack: [counter, ROOT_0, ..., NEW_ACCOUNT_HASH_i, account_id_i , ...]
+
+        # Move counter down for next iteration
+        movdn.9
+        # => [ROOT_i, NEW_ACCOUNT_HASH_i, account_id_i, counter, ...]
+
+        # Move root down (for smt64.set)
+        movdn.8 movdn.8 movdn.8 movdn.8
+        # => [NEW_ACCOUNT_HASH_i, account_id_i, ROOT_i, counter, ...]
+
+        # set new value in SMT
+        smt64.set dropw
+        # => [ROOT_{i+1}, counter, ...]
+
+        # loop counter
+        movup.4 sub.1 dup neq.0
+        # => [0 or 1, counter-1, ROOT_{i+1}, ...]
     end
+end
 ";
 
-/// Takes an iterator of (account id, node hash, Merkle path)
+/// `current_account_states`: iterator of (account id, node hash, Merkle path)
+/// `account_updates`: iterator of (account id, new account hash)
 pub fn compute_new_account_root(
-    accounts: impl Iterator<(AccountId, Digest, MerklePath)>
+    current_account_states: impl Iterator<Item = (AccountId, Digest, MerklePath)>,
+    account_updates: impl Iterator<Item = (AccountId, Digest)>,
 ) -> Digest {
     let account_program = {
         let assembler = Assembler::default()
@@ -22,7 +49,7 @@ pub fn compute_new_account_root(
             .expect("failed to load std-lib");
 
         assembler
-            .compile(ACCOUNT_PROGRAM_SOURCE)
+            .compile(ACCOUNT_UPDATE_ROOT_MASM)
             .expect("failed to load account update program")
     };
 
@@ -30,7 +57,7 @@ pub fn compute_new_account_root(
         let advice_inputs = {
             let merkle_store =
                 MerkleStore::default()
-                    .add_merkle_paths(accounts.map(|(account_id, node_hash, path)| {
+                    .add_merkle_paths(current_account_states.map(|(account_id, node_hash, path)| {
                         (u64::from(account_id), node_hash, path)
                     }))
                     .expect("Account SMT has depth 64; all keys are valid");
@@ -42,6 +69,8 @@ pub fn compute_new_account_root(
 
         DefaultHost::new(advice_provider)
     };
+
+    // TODO: Stack inputs
 
     todo!()
 }
