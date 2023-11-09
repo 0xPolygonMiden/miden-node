8af7e944641fee34e1a8bec9e31137b1d0ba1e90
Philippe Laferriere <plafer@protonmail.com>
2023-11-09 09:23:42 -0500

    tests comment

--- block-producer/src/block_builder/tests.rs
+++ block-producer/src/block_builder/tests.rs
@@ -1,2 +1,8 @@
-// tests
-// 1.
+// prover tests
+// 1. account validation works
+// 2. `BlockProver::compute_account_root()` works
+//   + make the updates outside the VM, and compare root
+
+// block builder tests (higher level)
+// 1. `apply_block()` is called
+// 2. if `apply_block()` fails, you fail too
