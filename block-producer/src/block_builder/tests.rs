a04bc2b33dfbdb5df01cbd2b70e0d4a3df5cc02c
Philippe Laferriere <plafer@protonmail.com>
2023-11-10 13:57:13 -0500

    test_build_block_failure

--- block-producer/src/block_builder/tests.rs
+++ block-producer/src/block_builder/tests.rs
@@ -2,12 +2,11 @@ use std::collections::BTreeSet;
 
 // block builder tests (higher level)
 // 1. `apply_block()` is called
-// 2. if `apply_block()` fails, you fail too
 use super::*;
 
 use miden_air::Felt;
 
-use crate::test_utils::MockStoreSuccess;
+use crate::test_utils::{MockStoreFailure, MockStoreSuccess};
 
 /// Tests that `build_block()` succeeds when the transaction batches are empty
 #[tokio::test]
@@ -26,4 +25,17 @@ async fn test_apply_block_called_empty_batches() {
 
     // Ensure that the store's `apply_block()` was called
     assert_eq!(*store.num_apply_block_called.read().await, 1);
+}
+
+/// Tests that `build_block()` fails when `get_block_inputs()` fails
+#[tokio::test]
+async fn test_build_block_failure() {
+    let store = Arc::new(MockStoreFailure::default());
+
+    let block_builder = DefaultBlockBuilder::new(store.clone());
+
+    let result = block_builder.build_block(Vec::new()).await;
+
+    // Ensure that the store's `apply_block()` was called
+    assert!(matches!(result, Err(BuildBlockError::GetBlockInputsFailed(_))));
 }
