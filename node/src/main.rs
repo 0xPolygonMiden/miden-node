a14cc4dcbf96014d21e3d5b12c3a45c11e7dce10
Philippe Laferriere <plafer@protonmail.com>
2023-12-07 16:36:35 -0500

    start rpc

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,9 +1,12 @@
-use std::{ops::Deref, path::PathBuf};
+use std::{path::PathBuf, time::Duration};
 
+use miden_node_rpc::config as rpc_config;
+use miden_node_rpc::config::RpcConfig;
+use miden_node_rpc::server as rpc_server;
 use miden_node_store::{
     config::{self as store_config, StoreConfig},
     db::Db,
-    server,
+    server as store_server,
 };
 use miden_node_utils::Config;
 use tokio::task::JoinSet;
@@ -16,13 +19,29 @@ async fn main() -> anyhow::Result<()> {
 
     // start store
     {
-        let store_config = PathBuf::from(store_config::CONFIG_FILENAME);
+        let config: StoreConfig = {
+            let config_path = PathBuf::from(store_config::CONFIG_FILENAME);
 
-        let config: StoreConfig =
-            StoreConfig::load_config(Some(store_config).as_deref()).extract()?;
+            StoreConfig::load_config(Some(config_path).as_deref()).extract()?
+        };
+
         let db = Db::get_conn(config.clone()).await?;
 
-        join_set.spawn(server::api::serve(config, db));
+        join_set.spawn(store_server::api::serve(config, db));
+    }
+
+    // wait for store to be started
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    // start rpc
+    {
+        let config: RpcConfig = {
+            let config_path = PathBuf::from(rpc_config::CONFIG_FILENAME);
+
+            RpcConfig::load_config(Some(config_path).as_deref()).extract()?
+        };
+
+        join_set.spawn(rpc_server::api::serve(config));
     }
 
     Ok(())
