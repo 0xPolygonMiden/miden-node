fc110538955ecf3763c7b8ca0fe8ba113a239e7f
Philippe Laferriere <plafer@protonmail.com>
2023-12-08 17:07:02 -0500

    genesis block header: use timestamp = 0

--- store/src/genesis.rs
+++ store/src/genesis.rs
@@ -1,18 +1,11 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-
 use miden_crypto::merkle::{EmptySubtreeRoots, MmrPeaks};
 use miden_node_proto::block_header;
 use miden_objects::{notes::NOTE_LEAF_DEPTH, Digest};
 
 use crate::state::ACCOUNT_DB_DEPTH;
 
-/// Generates the header of the genesis block. The timestamp is taken from the current system time.
+/// Generates the header of the genesis block. The timestamp is currently hardcoded to be the UNIX epoch.
 pub fn genesis_header() -> block_header::BlockHeader {
-    let now = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("failed to read system time")
-        .as_millis();
-
     block_header::BlockHeader {
         prev_hash: Some(Digest::default().into()),
         block_num: 0,
@@ -23,6 +16,6 @@ pub fn genesis_header() -> block_header::BlockHeader {
         batch_root: Some(Digest::default().into()),
         proof_hash: Some(Digest::default().into()),
         version: 0,
-        timestamp: now as u64,
+        timestamp: 0,
     }
 }
