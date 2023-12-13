31edee08eecaabfcb5b68a9381ae4fd57702cc71
Philippe Laferriere <plafer@protonmail.com>
2023-12-13 15:12:16 -0500

    add cli with clap

--- utilities/genesis/src/main.rs
+++ utilities/genesis/src/main.rs
@@ -4,24 +4,42 @@
 mod state;
 
 use anyhow::anyhow;
+use clap::Parser;
 use std::{fs::File, io::Write, path::Path};
 
 use state::GenesisState;
 
-const FILE_LOC: &'static str = "genesis.json";
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to output json file
+    #[arg(short, long, default_value = "genesis.json")]
+    output_path: String,
+
+    /// Generate the output file even if a file already exists
+    #[arg(short, long)]
+    force: bool,
+}
 
 fn main() -> anyhow::Result<()> {
-    let json_file_path = Path::new(FILE_LOC);
+    let args = Args::parse();
 
-    match json_file_path.try_exists() {
-        Ok(file_exists) => {
-            if file_exists {
-                return Err(anyhow!("Failed to generate new genesis file \"{FILE_LOC}\" because it already exists. Use the --force flag to overwrite."));
-            }
-        },
-        Err(err) => {
-            return Err(anyhow!("Failed to generate new genesis file \"{FILE_LOC}\". Error: {err}"));
-        },
+    let json_file_path = Path::new(&args.output_path);
+
+    if !args.force {
+        match json_file_path.try_exists() {
+            Ok(file_exists) => {
+                if file_exists {
+                    return Err(anyhow!("Failed to generate new genesis file \"{}\" because it already exists. Use the --force flag to overwrite.", args.output_path));
+                }
+            },
+            Err(err) => {
+                return Err(anyhow!(
+                    "Failed to generate new genesis file \"{}\". Error: {err}",
+                    args.output_path
+                ));
+            },
+        }
     }
 
     let genesis_state_json = {
@@ -30,7 +48,7 @@ fn main() -> anyhow::Result<()> {
         serde_json::to_string_pretty(&genesis_state).expect("Failed to serialize genesis state")
     };
 
-    let mut file = File::create(FILE_LOC)?;
+    let mut file = File::create(json_file_path)?;
     writeln!(file, "{}", genesis_state_json)?;
 
     Ok(())
