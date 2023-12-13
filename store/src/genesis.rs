5484087263ed4283c8ade9151e65716f32fb2e72
Philippe Laferriere <plafer@protonmail.com>
2023-12-13 16:59:57 -0500

    add version and timestamp to GenesisState

--- store/src/genesis.rs
+++ store/src/genesis.rs
@@ -14,78 +14,72 @@ use serde::{Deserialize, Serialize};
 
 use crate::state::ACCOUNT_DB_DEPTH;
 
-/// Generates the header of the genesis block. The timestamp is currently hardcoded to be the UNIX epoch.
-fn genesis_header() -> block_header::BlockHeader {
-    block_header::BlockHeader {
-        prev_hash: Some(Digest::default().into()),
-        block_num: 0,
-        chain_root: Some(MmrPeaks::new(0, Vec::new()).unwrap().hash_peaks().into()),
-        account_root: Some(EmptySubtreeRoots::entry(ACCOUNT_DB_DEPTH, 0).into()),
-        nullifier_root: Some(TieredSmt::default().root().into()),
-        note_root: Some(EmptySubtreeRoots::entry(NOTE_LEAF_DEPTH, 0).into()),
-        batch_root: Some(Digest::default().into()),
-        proof_hash: Some(Digest::default().into()),
-        version: 0,
-        timestamp: 0,
-    }
-}
-
 /// Represents the state at genesis, which will be used to derive the genesis block.
 #[derive(Serialize, Deserialize)]
 pub struct GenesisState {
     pub accounts: Vec<Account>,
+    pub version: u64,
+    pub timestamp: u64,
 }
 
 impl GenesisState {
     pub fn new(pub_key: rpo_falcon512::PublicKey) -> Self {
-        let mut accounts = Vec::new();
+        let accounts = {
+            let mut accounts = Vec::new();
 
-        // fungible asset faucet
-        {
-            let (account, _) = create_basic_fungible_faucet(
-                [0; 32],
-                TokenSymbol::new("TODO").unwrap(),
-                9,
-                Felt::from(1_000_000_000_u64),
-                AuthScheme::RpoFalcon512 { pub_key },
-            )
-            .unwrap();
+            // fungible asset faucet
+            {
+                let (account, _) = create_basic_fungible_faucet(
+                    [0; 32],
+                    TokenSymbol::new("TODO").unwrap(),
+                    9,
+                    Felt::from(1_000_000_000_u64),
+                    AuthScheme::RpoFalcon512 { pub_key },
+                )
+                .unwrap();
 
-            accounts.push(account);
+                accounts.push(account);
+            }
+
+            // basic wallet account
+            {
+                let (account, _) = create_basic_wallet(
+                    [0; 32],
+                    AuthScheme::RpoFalcon512 { pub_key },
+                    miden_objects::accounts::AccountType::RegularAccountUpdatableCode,
+                )
+                .unwrap();
+
+                accounts.push(account);
+            }
+
+            accounts
+        };
+
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("we are after 1970")
+            .as_millis() as u64;
+
+        Self {
+            accounts,
+            version: 0,
+            timestamp,
         }
-
-        // basic wallet account
-        {
-            let (account, _) = create_basic_wallet(
-                [0; 32],
-                AuthScheme::RpoFalcon512 { pub_key },
-                miden_objects::accounts::AccountType::RegularAccountUpdatableCode,
-            )
-            .unwrap();
-
-            accounts.push(account);
-        }
-
-        Self { accounts }
     }
 }
 
 impl TryFrom<GenesisState> for block_header::BlockHeader {
     type Error = MerkleError;
 
-    fn try_from(value: GenesisState) -> Result<Self, Self::Error> {
+    fn try_from(genesis_state: GenesisState) -> Result<Self, Self::Error> {
         let account_smt = SimpleSmt::with_leaves(
             ACCOUNT_DB_DEPTH,
-            value
+            genesis_state
                 .accounts
                 .into_iter()
                 .map(|account| (account.id().into(), account.hash().into())),
         )?;
-
-        let timestamp = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .expect("we are after 1970")
-            .as_millis() as u64;
 
         let block_header = BlockHeader::new(
             Digest::default(),
@@ -96,9 +90,9 @@ impl TryFrom<GenesisState> for block_header::BlockHeader {
             *EmptySubtreeRoots::entry(NOTE_LEAF_DEPTH, 0),
             Digest::default(),
             Digest::default(),
-            Felt::from(0_u64),
-            // FIXME: timestamp and version goes in json
-            timestamp.into(),
+            genesis_state.version.into(),
+            // FIXME: timestamp and versioddn goes in json
+            genesis_state.timestamp.into(),
         );
 
         Ok(block_header.into())
