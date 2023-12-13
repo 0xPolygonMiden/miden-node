e083e432b8ba5fc542ca233621fbbe21f2d80d82
Philippe Laferriere <plafer@protonmail.com>
2023-12-13 14:27:05 -0500

    main

--- utilities/genesis/src/main.rs
+++ utilities/genesis/src/main.rs
@@ -1,21 +1,36 @@
 //! Generates a JSON file representing the chain state at genesis. This information will be used to
 //! derive the genesis block.
 
-use miden_objects::accounts::Account;
-use serde::{Deserialize, Serialize};
+mod state;
 
-/// Represents the state at genesis, which will be used to derive the genesis block.
-#[derive(Serialize, Deserialize)]
-pub struct GenesisState {
-    pub accounts: Vec<Account>,
-}
+use anyhow::anyhow;
+use std::{fs::write, path::Path};
 
-impl Default for GenesisState {
-    fn default() -> Self {
-        let accounts = Vec::new();
+use state::GenesisState;
 
-        Self { accounts }
+const FILE_LOC: &'static str = "genesis.json";
+
+fn main() -> anyhow::Result<()> {
+    let json_file_path = Path::new(FILE_LOC);
+
+    match json_file_path.try_exists() {
+        Ok(existence_verified) => {
+            if !existence_verified {
+                return Err(anyhow!("Failed to generate new genesis file at {FILE_LOC}: there are broken symbolic links along the path"));
+            }
+        },
+        Err(err) => {
+            return Err(anyhow!("Failed to generate new genesis file at {FILE_LOC}. Error: {err}. Use the --force flag to overwrite."));
+        },
     }
-}
 
-fn main() {}
+    let genesis_state_json = {
+        let genesis_state = GenesisState::default();
+
+        serde_json::to_string(&genesis_state).expect("Failed to serialize genesis state")
+    };
+
+    write(FILE_LOC, genesis_state_json)?;
+
+    Ok(())
+}
