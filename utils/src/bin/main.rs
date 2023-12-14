33f71b83c3c296f23b24f97f1cc0e57e55aa52b7
Philippe Laferriere <plafer@protonmail.com>
2023-12-14 10:48:59 -0500

    pull magic values in constants

--- utils/src/bin/main.rs
+++ utils/src/bin/main.rs
@@ -1,6 +1,12 @@
 //! Generates a JSON file representing the chain state at genesis. This information will be used to
 //! derive the genesis block.
 
+use anyhow::anyhow;
+use clap::Parser;
+use miden_crypto::{dsa::rpo_falcon512::PublicKey, Felt};
+use miden_lib::{faucets::create_basic_fungible_faucet, wallets::create_basic_wallet, AuthScheme};
+use miden_node_utils::genesis::GenesisState;
+use miden_objects::assets::TokenSymbol;
 use std::{
     fs::File,
     io::Write,
@@ -8,18 +14,29 @@ use std::{
     time::{SystemTime, UNIX_EPOCH},
 };
 
-use anyhow::anyhow;
-use clap::Parser;
-use miden_crypto::{dsa::rpo_falcon512::PublicKey, Felt};
-use miden_lib::{faucets::create_basic_fungible_faucet, wallets::create_basic_wallet, AuthScheme};
-use miden_node_utils::genesis::GenesisState;
-use miden_objects::assets::TokenSymbol;
+// CONSTANTS
+// =================================================================================================
+
+/// Token symbol of the faucet present at genesis
+const FUNGIBLE_FAUCET_TOKEN_SYMBOL: &'static str = "POL";
+
+/// Decimals for the token of the faucet present at genesis
+const FUNGIBLE_FAUCET_TOKEN_DECIMALS: u8 = 9;
+
+/// Max supply for the token of the faucet present at genesis
+const FUNGIBLE_FAUCET_TOKEN_MAX_SUPPLY: u64 = 1_000_000_000;
+
+/// Default path at which the genesis file will be written to
+const DEFAULT_GENESIS_FILE_PATH: &'static str = "genesis.json";
+
+// MAIN
+// =================================================================================================
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Path to output json file
-    #[arg(short, long, default_value = "genesis.json")]
+    #[arg(short, long, default_value = DEFAULT_GENESIS_FILE_PATH)]
     output_path: String,
 
     /// Generate the output file even if a file already exists
@@ -57,9 +74,9 @@ fn main() -> anyhow::Result<()> {
             {
                 let (account, _) = create_basic_fungible_faucet(
                     [0; 32],
-                    TokenSymbol::new("TODO").unwrap(),
-                    9,
-                    Felt::from(1_000_000_000_u64),
+                    TokenSymbol::new(FUNGIBLE_FAUCET_TOKEN_SYMBOL).unwrap(),
+                    FUNGIBLE_FAUCET_TOKEN_DECIMALS,
+                    Felt::from(FUNGIBLE_FAUCET_TOKEN_MAX_SUPPLY),
                     AuthScheme::RpoFalcon512 { pub_key },
                 )
                 .unwrap();
@@ -70,7 +87,7 @@ fn main() -> anyhow::Result<()> {
             // basic wallet account
             {
                 let (account, _) = create_basic_wallet(
-                    [0; 32],
+                    [1; 32],
                     AuthScheme::RpoFalcon512 { pub_key },
                     miden_objects::accounts::AccountType::RegularAccountUpdatableCode,
                 )
