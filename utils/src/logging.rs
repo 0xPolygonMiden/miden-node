241c832f060c9dc63de3004a6ce242f4cc853df8
Philippe Laferriere <plafer@protonmail.com>
2023-12-20 14:53:47 -0500

    change logging

--- utils/src/logging.rs
+++ utils/src/logging.rs
@@ -4,14 +4,12 @@ use tracing_subscriber::{self, fmt::format::FmtSpan, EnvFilter};
 
 pub fn setup_logging() -> Result<()> {
     let subscriber = tracing_subscriber::fmt()
-        .json()
+        .pretty()
         .compact()
         .with_level(true)
         .with_file(true)
         .with_line_number(true)
-        .with_thread_ids(false)
-        .with_thread_names(true)
-        .with_target(true)
+        .with_target(false)
         .with_env_filter(EnvFilter::from_default_env())
         .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
         .finish();
