ebbd03097903d12b3bf323a053506e774002a4f3
Philippe Laferriere <plafer@protonmail.com>
2023-12-21 11:19:35 -0500

    load_config: only consider env vars and path

--- utils/src/config.rs
+++ utils/src/config.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 
 use directories;
 use figment::{
-    providers::{Env, Format, Serialized, Toml},
+    providers::{Env, Format, Toml},
     Figment,
 };
 use serde::{Deserialize, Serialize};
@@ -48,16 +48,42 @@ pub trait Config: Default + Serialize {
     ///
     /// This function will look on the following places, from lowest to higest priority:
     ///
-    /// - Code defaults
-    /// - User's configuration file on the system's default locations.
-    /// - Configuration file on the current directory or the config file provided via CLI arg.
+    /// - Configuration file at the provided path. If the path is relative, searches in parent
+    ///   directories all the way to the root as well.
     /// - Environment variables.
     ///
     /// The above configuration options are indented to support easy of packaging and deployment.
     fn load_config(config_file: Option<&Path>) -> Figment {
-        Figment::from(Serialized::defaults(Self::default()))
-            .merge(Self::load_user_config().unwrap_or_default())
-            .merge(Self::load_local_config(config_file))
-            .merge(Self::load_env_config())
+        let env_figment = Self::load_env_config();
+
+        match config_file {
+            // Note: `Figment::join()` gives precedence to environment variables
+            Some(config_file) => env_figment.join(Toml::file(config_file)),
+            None => env_figment,
+        }
     }
+}
+
+#[test]
+fn f() {
+    #[derive(serde::Deserialize, Debug)]
+    struct C {
+        name: String
+    }
+
+    impl std::fmt::Display for C {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            writeln!(f, "{}", self.name)
+        }
+    }
+
+    let x = Figment::from(Toml::file("doesnt_exit"));
+    
+    let c: Result<C, figment::Error> = x.extract();
+
+    match c {
+        Ok(v) => println!("{v}"),
+        Err(e) => println!("{e}"),
+    }
+
 }
