499e76d1cb5425d8087df8e6574fc771aa235c5b
polydez <155382956+polydez@users.noreply.github.com>
2024-02-01 15:00:00 +0500

    fix(logs): addressed review comments

--- block-producer/src/batch_builder/mod.rs
+++ block-producer/src/batch_builder/mod.rs
@@ -80,7 +80,7 @@ where
     // --------------------------------------------------------------------------------------------
 
     /// TODO: add comments
-    #[instrument(target = "miden-block-producer", name = "batch_builder" skip_all)]
+    #[instrument(target = "miden-block-producer", name = "block_producer" skip_all)]
     pub async fn run(self: Arc<Self>) {
         let mut interval = time::interval(self.options.block_frequency);
 
