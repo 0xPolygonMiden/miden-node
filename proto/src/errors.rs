5ea077022eee3f6cf3646994ede1ae38f9e4a39d
Philippe Laferriere <plafer@protonmail.com>
2024-02-06 16:40:07 -0500

    `SmtLeaf` conversion

--- proto/src/errors.rs
+++ proto/src/errors.rs
@@ -1,6 +1,4 @@
-use std::any::type_name;
-
-use miden_crypto::merkle::MmrError;
+use miden_crypto::merkle::{MmrError, SmtLeafError};
 use thiserror::Error;
 
 #[derive(Error, Debug, Clone, PartialEq)]
@@ -19,22 +17,8 @@ pub enum ParseError {
     NotAValidFelt,
     #[error("Received TSMT proof is invalid")]
     InvalidProof,
-    #[error("Field `{field_name}` required to be filled in protobuf representation of {entity}")]
-    MissingFieldInProtobufRepresentation {
-        entity: &'static str,
-        field_name: &'static str,
-    },
-}
-
-pub trait MissingFieldHelper {
-    fn missing_field(field_name: &'static str) -> ParseError;
-}
-
-impl<T: prost::Message> MissingFieldHelper for T {
-    fn missing_field(field_name: &'static str) -> ParseError {
-        ParseError::MissingFieldInProtobufRepresentation {
-            entity: type_name::<T>(),
-            field_name,
-        }
-    }
+    #[error("Protobuf message missing data")]
+    ProtobufMissingData,
+    #[error("smt leaf error: {0}")]
+    SmtLeafError(#[from] SmtLeafError)
 }
