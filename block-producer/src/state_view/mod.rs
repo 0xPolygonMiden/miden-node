b281493e886bc5706dc41ad72ff2968c928b75cd
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2024-02-12 15:08:34 +0100

    fmt

--- block-producer/src/state_view/mod.rs
+++ block-producer/src/state_view/mod.rs
@@ -4,11 +4,10 @@ use async_trait::async_trait;
 use miden_node_proto::TransactionInputs;
 use miden_node_utils::formatting::format_array;
 use miden_objects::{accounts::AccountId, notes::Nullifier, transaction::InputNotes, Digest};
-use tokio::sync::RwLock;
-use tracing::{debug, instrument};
-
 #[cfg(not(test))]
 use miden_tx::TransactionVerifier;
+use tokio::sync::RwLock;
+use tracing::{debug, instrument};
 
 use crate::{
     block::Block,
