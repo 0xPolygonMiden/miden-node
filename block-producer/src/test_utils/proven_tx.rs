8c30119b3bda2c9da0155565cf8cb35fb41d846f
polydez <155382956+polydez@users.noreply.github.com>
2024-04-02 12:32:36 +0500

    fix: update to the latest `next`

--- block-producer/src/test_utils/proven_tx.rs
+++ block-producer/src/test_utils/proven_tx.rs
@@ -6,7 +6,7 @@ use miden_objects::{
     notes::{NoteEnvelope, NoteMetadata, NoteType, Nullifier},
     transaction::{ProvenTransaction, ProvenTransactionBuilder},
     vm::ExecutionProof,
-    Digest, Felt, Hasher, ONE,
+    Digest, Felt, Hasher, ONE, ZERO,
 };
 use winterfell::StarkProof;
 
