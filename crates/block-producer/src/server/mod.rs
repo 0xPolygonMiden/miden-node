13621965efd6b13d7eadb9ed22e85d0adc928053
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2024-04-18 17:19:54 +0400

    Added in store and block-producer

--- crates/block-producer/src/server/mod.rs
+++ crates/block-producer/src/server/mod.rs
@@ -61,8 +61,8 @@ pub async fn serve(config: BlockProducerConfig) -> Result<(), ApiError> {
         .to_socket_addrs()
         .map_err(ApiError::EndpointToSocketFailed)?
         .next()
-        .ok_or("Failed to resolve address.")
-        .map_err(|err| ApiError::AddressResolutionFailed(err.to_string()))?;
+        .ok_or(config.endpoint.to_string())
+        .map_err(|err| ApiError::AddressResolutionFailed(err))?;
 
     Server::builder()
         .add_service(block_producer)
