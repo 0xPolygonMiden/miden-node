d5a705500be4f236555bb4d657defd62b0c90472
tomyrd <chubyrd1@gmail.com>
2024-05-24 18:53:37 -0300

    Add FaucetAuthenticator

--- bin/faucet/src/client.rs
+++ bin/faucet/src/client.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{cell::RefCell, time::Duration};
 
 use miden_lib::{accounts::faucets::create_basic_fungible_faucet, AuthScheme};
 use miden_node_proto::generated::{
@@ -6,11 +6,11 @@ use miden_node_proto::generated::{
     rpc::api_client::ApiClient,
 };
 use miden_objects::{
-    accounts::{Account, AccountId, AccountStorageType},
+    accounts::{Account, AccountDelta, AccountId, AccountStorageType, AuthSecretKey},
     assembly::ModuleAst,
     assets::TokenSymbol,
     crypto::{
-        dsa::rpo_falcon512::SecretKey,
+        dsa::rpo_falcon512::{self, Polynomial, SecretKey},
         merkle::{MmrPeaks, PartialMmr},
     },
     notes::NoteId,
@@ -18,8 +18,8 @@ use miden_objects::{
     BlockHeader, Felt, Word,
 };
 use miden_tx::{
-    utils::Serializable, DataStore, DataStoreError, ProvingOptions, TransactionInputs,
-    TransactionProver,
+    utils::Serializable, AuthenticationError, DataStore, DataStoreError, ProvingOptions,
+    TransactionAuthenticator, TransactionInputs, TransactionProver,
 };
 use rand_chacha::{rand_core::SeedableRng, ChaCha20Rng};
 use tonic::transport::Channel;
@@ -28,6 +28,7 @@ use crate::{config::FaucetConfig, errors::FaucetError};
 
 pub struct FaucetClient {
     data_store: FaucetDataStore,
+    authenticator: FaucetAuthenticator,
     rpc_api: ApiClient<Channel>,
     config: FaucetConfig,
 }
@@ -85,10 +86,13 @@ impl FaucetClient {
             root_chain_mmr,
         );
 
+        let authenticator = FaucetAuthenticator::new(secret);
+
         Ok(Self {
             data_store,
             rpc_api,
             config: faucet_config,
+            authenticator,
         })
     }
 
@@ -171,4 +175,84 @@ impl DataStore for FaucetDataStore {
         let module_ast = self.faucet_account.code().module().clone();
         Ok(module_ast)
     }
+}
+
+struct FaucetAuthenticator {
+    faucet_secret_key: AuthSecretKey,
+    rng: RefCell<ChaCha20Rng>,
+}
+
+impl FaucetAuthenticator {
+    pub fn new(faucet_secret_key: SecretKey) -> Self {
+        Self {
+            faucet_secret_key: AuthSecretKey::RpoFalcon512(faucet_secret_key),
+            rng: RefCell::new(ChaCha20Rng::from_entropy()),
+        }
+    }
+}
+
+impl TransactionAuthenticator for FaucetAuthenticator {
+    fn get_signature(
+        &self,
+        _pub_key: Word,
+        message: Word,
+        _account_delta: &AccountDelta,
+    ) -> Result<Vec<Felt>, AuthenticationError> {
+        let mut rng = self.rng.borrow_mut();
+        let AuthSecretKey::RpoFalcon512(k) = &self.faucet_secret_key;
+        get_falcon_signature(k, message, &mut rng)
+    }
+}
+
+// HELPER FUNCTIONS
+// ================================================================================================
+
+// TODO: Remove the falcon signature function once it's available on base and made public
+
+/// Retrieves a falcon signature over a message.
+/// Gets as input a [Word] containing a secret key, and a [Word] representing a message and
+/// outputs a vector of values to be pushed onto the advice stack.
+/// The values are the ones required for a Falcon signature verification inside the VM and they are:
+///
+/// 1. The nonce represented as 8 field elements.
+/// 2. The expanded public key represented as the coefficients of a polynomial of degree < 512.
+/// 3. The signature represented as the coefficients of a polynomial of degree < 512.
+/// 4. The product of the above two polynomials in the ring of polynomials with coefficients
+/// in the Miden field.
+///
+/// # Errors
+/// Will return an error if either:
+/// - The secret key is malformed due to either incorrect length or failed decoding.
+/// - The signature generation failed.
+///
+/// TODO: once this gets made public in miden base, remve this implementation and use the one from
+/// base
+fn get_falcon_signature(
+    key: &rpo_falcon512::SecretKey,
+    message: Word,
+    rng: &mut ChaCha20Rng,
+) -> Result<Vec<Felt>, AuthenticationError> {
+    // Generate the signature
+    let sig = key.sign_with_rng(message, rng);
+    // The signature is composed of a nonce and a polynomial s2
+    // The nonce is represented as 8 field elements.
+    let nonce = sig.nonce();
+    // We convert the signature to a polynomial
+    let s2 = sig.sig_poly();
+    // We also need in the VM the expanded key corresponding to the public key the was provided
+    // via the operand stack
+    let h = key.compute_pub_key_poly().0;
+    // Lastly, for the probabilistic product routine that is part of the verification procedure,
+    // we need to compute the product of the expanded key and the signature polynomial in
+    // the ring of polynomials with coefficients in the Miden field.
+    let pi = Polynomial::mul_modulo_p(&h, s2);
+    // We now push the nonce, the expanded key, the signature polynomial, and the product of the
+    // expanded key and the signature polynomial to the advice stack.
+    let mut result: Vec<Felt> = nonce.to_elements().to_vec();
+
+    result.extend(h.coefficients.iter().map(|a| Felt::from(a.value() as u32)));
+    result.extend(s2.coefficients.iter().map(|a| Felt::from(a.value() as u32)));
+    result.extend(pi.iter().map(|a| Felt::new(*a)));
+    result.reverse();
+    Ok(result)
 }
