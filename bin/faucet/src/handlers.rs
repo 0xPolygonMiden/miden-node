57cc00367343030d7653b785e282e318044fbb96
Tomas Rodriguez Dala <43424983+tomyrd@users.noreply.github.com>
2024-06-07 03:38:50 -0300

    feat: remove client dependency from faucet (#368)

--- bin/faucet/src/handlers.rs
+++ bin/faucet/src/handlers.rs
@@ -1,21 +1,13 @@
 use actix_web::{get, http::header, post, web, HttpResponse, Result};
-use miden_client::{
-    client::transactions::transaction_request::TransactionTemplate, store::InputNoteRecord,
-};
 use miden_objects::{
     accounts::AccountId,
-    assets::FungibleAsset,
-    notes::{NoteId, NoteType},
-    transaction::OutputNote,
+    notes::{NoteDetails, NoteFile, NoteId},
     utils::serde::Serializable,
 };
 use serde::{Deserialize, Serialize};
 use tracing::info;
 
-use crate::{
-    errors::FaucetError,
-    utils::{build_client, FaucetState},
-};
+use crate::{errors::FaucetError, state::FaucetState};
 
 #[derive(Deserialize)]
 struct FaucetRequest {
@@ -34,7 +26,7 @@ struct FaucetMetadataReponse {
 pub async fn get_metadata(state: web::Data<FaucetState>) -> HttpResponse {
     let response = FaucetMetadataReponse {
         id: state.id.to_string(),
-        asset_amount_options: state.faucet_config.asset_amount_options.clone(),
+        asset_amount_options: state.config.asset_amount_options.clone(),
     };
 
     HttpResponse::Ok().json(response)
@@ -51,64 +43,34 @@ pub async fn get_tokens(
     );
 
     // Check that the amount is in the asset amount options
-    if !state.faucet_config.asset_amount_options.contains(&req.asset_amount) {
+    if !state.config.asset_amount_options.contains(&req.asset_amount) {
         return Err(FaucetError::BadRequest("Invalid asset amount.".to_string()).into());
     }
 
-    let client_config = state.faucet_config.clone();
-    let mut client = build_client(client_config.database_filepath, &client_config.node_url)?;
+    let mut client = state.client.lock().await;
 
     // Receive and hex user account id
     let target_account_id = AccountId::from_hex(req.account_id.as_str())
         .map_err(|err| FaucetError::BadRequest(err.to_string()))?;
 
-    // Instantiate asset
-    let asset = FungibleAsset::new(state.id, req.asset_amount)
-        .map_err(|err| FaucetError::InternalServerError(err.to_string()))?;
-
-    // Instantiate note type
-    let note_type = if req.is_private_note {
-        NoteType::OffChain
-    } else {
-        NoteType::Public
-    };
-
-    // Instantiate transaction template
-    let tx_template = TransactionTemplate::MintFungibleAsset(asset, target_account_id, note_type);
-
-    // Instantiate transaction request
-    let tx_request = client
-        .build_transaction_request(tx_template)
-        .map_err(|err| FaucetError::InternalServerError(err.to_string()))?;
-
-    // Run transaction executor & execute transaction
-    let tx_result = client
-        .new_transaction(tx_request)
-        .map_err(|err| FaucetError::InternalServerError(err.to_string()))?;
-
-    // Get created notes from transaction result
-    let created_notes = tx_result.created_notes().clone();
+    // Execute transaction
+    info!("Executing mint transaction for account.");
+    let (executed_tx, created_note) = client.execute_mint_transaction(
+        target_account_id,
+        req.is_private_note,
+        req.asset_amount,
+    )?;
 
     // Run transaction prover & send transaction to node
-    client
-        .submit_transaction(tx_result)
-        .await
-        .map_err(|err| FaucetError::InternalServerError(err.to_string()))?;
+    info!("Proving and submitting transaction.");
+    client.prove_and_submit_transaction(executed_tx).await?;
 
-    let note_id: NoteId;
+    let note_id: NoteId = created_note.id();
+    let note_details =
+        NoteDetails::new(created_note.assets().clone(), created_note.recipient().clone());
 
     // Serialize note into bytes
-    let bytes = match created_notes.get_note(0) {
-        OutputNote::Full(note) => {
-            note_id = note.id();
-            InputNoteRecord::from(note.clone()).to_bytes()
-        },
-        OutputNote::Header(_) => {
-            return Err(
-                FaucetError::InternalServerError("Failed to generate note.".to_string()).into()
-            )
-        },
-    };
+    let bytes = NoteFile::NoteDetails(note_details).to_bytes();
 
     info!("A new note has been created: {}", note_id);
 
