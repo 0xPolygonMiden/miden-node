fd7be8f59dc07b45ea1938f3abf6c4af0601bdee
Ignacio Amigo <ignacio.amigo@lambdaclass.com>
2024-06-25 13:57:37 -0300

    Address reviews

--- crates/rpc-proto/README.md
+++ crates/rpc-proto/README.md
@@ -1,6 +1,6 @@
-# Miden node utils
+# Miden RPC proto
 
-This crate contains the RPC protobuf message definitions.
+This crate contains protobuf message definitions of the RPC component of the Miden node.
 It consists of a map of `(filename, file contents)` where each entry refers to a protobuf file.
 
 Additionally, the crate exposes a `write_proto(target_dir)` function that writes the files into `target_dir`.
--- crates/rpc-proto/src/lib.rs
+++ crates/rpc-proto/src/lib.rs
@@ -6,7 +6,7 @@ pub use proto_files::PROTO_FILES;
 /// Writes the RPC protobuf file into `target_dir`.
 pub fn write_proto(target_dir: &Path) -> Result<(), String> {
     if !target_dir.exists() {
-        fs::create_dir_all(target_dir).unwrap();
+        fs::create_dir_all(target_dir).map_err(|e| format!("Error creating directory: {}" e.to_string()))?;
     } else if !target_dir.is_dir() {
         return Err("The target path exists but is not a directory".to_string());
     }
@@ -14,8 +14,8 @@ pub fn write_proto(target_dir: &Path) -> Result<(), String> {
     for (file_name, file_content) in PROTO_FILES {
         let mut file_path = target_dir.to_path_buf();
         file_path.push(file_name);
-        let mut file = File::create(&file_path).unwrap();
-        file.write_all(file_content.as_bytes()).unwrap();
+        let mut file = File::create(&file_path).map_err(|e| format!("Error creating file: {}" e.to_string()))?;
+        file.write_all(file_content.as_bytes()).map_err(|e| format!("Error writing file: {}" e.to_string()))?;
     }
 
     Ok(())
