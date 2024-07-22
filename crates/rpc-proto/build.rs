1769869be6c780b721dedf90b58b73567ceb82b6
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2024-07-22 11:26:12 +0200

    feat: improve proto build scripts

--- crates/rpc-proto/build.rs
+++ crates/rpc-proto/build.rs
@@ -22,13 +22,13 @@ const DOC_COMMENT: &str =
 /// This is done only if BUILD_PROTO environment variable is set to `1` to avoid running the script
 /// on crates.io where repo-level .proto files are not available.
 fn main() -> io::Result<()> {
-    println!("cargo:rerun-if-changed=proto");
-    println!("cargo:rerun-if-changed=../../proto");
-
     // skip this build script in BUILD_PROTO environment variable is not set to `1`
     if env::var("BUILD_PROTO").unwrap_or("0".to_string()) == "0" {
         return Ok(());
     }
+
+    println!("cargo:rerun-if-changed=proto");
+    println!("cargo:rerun-if-changed=../../proto");
 
     // copy all .proto files into this crate. all these files need to be local to the crate to
     // publish the crate to crates.io
