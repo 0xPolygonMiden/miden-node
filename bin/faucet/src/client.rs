0be78c7be1f6d16795a1280421780230ac8d0356
Ignacio Amigo <ignacio.amigo@lambdaclass.com>
2024-09-17 19:42:35 -0300

    Remove error trace on faucet

--- bin/faucet/src/client.rs
+++ bin/faucet/src/client.rs
@@ -29,7 +29,6 @@ use miden_tx::{
 use rand::{rngs::StdRng, thread_rng, Rng};
 use rand_chacha::{rand_core::SeedableRng, ChaCha20Rng};
 use tonic::transport::Channel;
-use tracing::error;
 
 use crate::{config::FaucetConfig, errors::FaucetError};
 
@@ -114,7 +113,6 @@ impl FaucetClient {
             .executor
             .execute_transaction(self.id, 0, &[], transaction_args)
             .map_err(|err| {
-                error!("{err}");
                 FaucetError::InternalServerError(format!("Failed to execute transaction: {}", err))
             })?;
 
