555117aba633d4c26d28cb362bacbf65805e2ac7
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2024-09-18 18:26:08 +0200

    end of day

--- crates/block-producer/src/tx_pool/mod.rs
+++ crates/block-producer/src/tx_pool/mod.rs
@@ -1,5 +1,8 @@
+#![allow(unused)]
+
 use std::{
     collections::{BTreeMap, BTreeSet},
+    fmt::Display,
     sync::Arc,
 };
 
@@ -12,10 +15,28 @@ use miden_objects::{
 use crate::store::TransactionInputs;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct BatchId(u64);
+pub struct BatchId(u64);
+
+impl Display for BatchId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct BlockNumber(u32);
+
+impl Display for BlockNumber {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
 
 pub struct TransactionPool {
-    account_state: BTreeMap<AccountId, Digest>,
+    account_state: BTreeMap<AccountId, (Digest, TransactionId)>,
+
+    /// Block number at which transaction inputs are considered stale.
+    stale_block: BlockNumber,
 
     /// All transactions currently inflight. This includes those not yet processed, those in batches and those in an inflight block.
     tx_pool: BTreeMap<TransactionId, Transaction>,
@@ -59,7 +80,7 @@ impl TransactionPool {
 
             // Check if any of the child transactions are now rootable.
             for child in children {
-                self.check_rootable(child);
+                self.try_root_transaction(child);
             }
 
             // Track batch dependencies.
@@ -92,42 +113,83 @@ impl TransactionPool {
         Some((batch_id, batch))
     }
 
+    /// Drops the failed batch and all of its descendents.
+    ///
+    /// Transactions are placed back in the queue.
+    ///
+    /// Complete afaik.
     pub fn batch_failed(&mut self, batch: BatchId) {
-        let mut to_remove = vec![batch];
-        let mut txs = Vec::new();
+        // Skip non-existent batches. Its possible for this batch to have been
+        // removed as a descendent of a previously failed batch.
+        if !self.batch_pool.contains_key(&batch) {
+            tracing::debug!(%batch, "Ignoring unknwon failed batch");
+            return;
+        }
 
-        while let Some(batch_id) = to_remove.pop() {
-            if let Some((children, batch_txs)) = self.inflight_batches.remove(&batch_id) {
-                to_remove.extend(children);
-                txs.extend(batch_txs.into_iter());
-            } else {
-                tracing::debug!(?batch_id, "Ignoring batch as it is not an inflight batch.")
-            }
+        let (batches, mut transactions) = self.batch_descendents(batch);
+
+        // Drop all impacted batches.
+        //
+        // We could also re-attempt the batch but right now we don't have
+        // the information yet to make such a call.
+        for batch in &batches {
+            self.batch_pool.remove(batch);
         }
 
         // Mark all transactions as back in-queue.
-        for tx in &txs {
+        for tx in &transactions {
             self.tx_pool.get_mut(tx).expect("Transaction must be in pool").status =
                 TransactionStatus::InQueue;
         }
 
+        let impacted_transactions = transactions.len();
+
         // Check all transactions as possible roots. We also need to recheck the current roots as
         // they may have become invalid roots.
-        txs.extend(self.tx_roots.clone().into_iter());
+        transactions.extend(self.tx_roots.clone().into_iter());
         self.tx_roots.clear();
-        for tx in txs {
-            self.check_rootable(tx);
+        for tx in transactions {
+            self.try_root_transaction(tx);
         }
+
+        tracing::warn!(%batch, descendents=?batches, %impacted_transactions, "Batch failed, dropping all inflight descendent batches, impacted transactions are back in queue.");
+    }
+
+    /// Returns the descendents of a batch that are in the pool i.e. children, grandchildren, etc.
+    /// including their transactions.
+    ///
+    /// The input batch is included.
+    ///
+    /// Complete afaik.
+    fn batch_descendents(&self, batch: BatchId) -> (BTreeSet<BatchId>, BTreeSet<TransactionId>) {
+        let mut to_process = vec![batch];
+        let mut descendents = BTreeSet::new();
+        let mut transactions = BTreeSet::new();
+
+        while let Some(batch) = to_process.pop() {
+            // Guard against repeat processing. This is possible because a batch can have multiple parents.
+            if descendents.insert(batch) {
+                let batch = self.batch_pool.get(&batch).expect("Batch should exist");
+
+                transactions.extend(&batch.transactions);
+                to_process.extend(&batch.children);
+            }
+        }
+
+        (descendents, transactions)
     }
 
     /// Adds a transaction to the set of roots IFF all of its parent's are no longer in-queue.
-    pub fn check_rootable(&mut self, tx_id: TransactionId) {
+    ///
+    /// Naming is hard. What should this be called?
+    ///
+    /// Complete.
+    fn try_root_transaction(&mut self, tx_id: TransactionId) {
         if let Some(tx) = self.tx_pool.get(&tx_id) {
             for parent in &tx.parents {
-                let parent_status =
-                    self.tx_pool.get(&parent).expect("Parent must be in pool still").status;
+                let parent = self.tx_pool.get(&parent).expect("Parent must be in pool still");
 
-                if parent_status == TransactionStatus::InQueue {
+                if parent.status == TransactionStatus::InQueue {
                     return;
                 }
             }
@@ -137,48 +199,78 @@ impl TransactionPool {
         }
     }
 
-    pub fn batch_complete(&mut self, batch: BatchId) {
-        // TODO: the rest of the owl.
-        if 
+    pub fn batch_complete(&mut self, batch_id: BatchId) {
+        let Some(batch) = self.batch_pool.get_mut(&batch_id) else {
+            tracing::warn!(%batch_id, "Ignoring unknown completed batch.");
+            return;
+        };
+
+        batch.status = BatchStatus::Complete;
+
+        // Add the batch as a root if all parents are complete. No wait this is wrong -- update me tomorrow!!!!
+        // Should only be a root if all parents are inflight in a block.
+        for parent in &batch.parents.clone() {
+            let parent = self.batch_pool.get(parent).expect("Parent batch must be in pool");
+
+            if parent.status != BatchStatus::Complete {
+                return;
+            }
+        }
+        self.batch_roots.insert(batch_id);
     }
 
+    /// Complete barring todos.
     pub fn add_transaction(
         mut self,
         transaction: ProvenTransaction,
-        inputs: TransactionInputs,
+        mut inputs: TransactionInputs,
     ) -> Result<(), AddTransactionError> {
-        // TODO: verify that transaction inputs are not stale.
+        if inputs.current_block_height <= self.stale_block.0 {
+            return Err(AddTransactionError::StaleInputs {
+                input_block: BlockNumber(inputs.current_block_height),
+                stale_limit: self.stale_block,
+            });
+        }
 
-        // Validate inflight state.
         let account_update = transaction.account_update();
-        // The current account state hash.
-        let account_state = self
-            .account_state
-            .get(&account_update.account_id())
-            .cloned()
-            .or(inputs.account_hash)
-            .unwrap_or_default();
+        let mut parents = BTreeSet::new();
 
-        if account_state != account_update.init_state_hash() {
+        // Merge inflight state with inputs.
+        //
+        // This gives us the latest applicable state for this transaction.
+        // TODO: notes and nullifiers.
+        if let Some((state, parent)) = self.account_state.get(&account_update.account_id()) {
+            parents.insert(*parent);
+            inputs.account_hash = Some(*state);
+        }
+
+        if inputs.account_hash.unwrap_or_default() != account_update.init_state_hash() {
             return Err(AddTransactionError::InvalidAccountState {
-                current: account_state,
+                current: inputs.account_hash.unwrap_or_default(),
                 expected: account_update.init_state_hash(),
             });
         }
 
-        // TODO: notes and nullifiers.
-
         // Transaction is valid, update inflight state.
-        self.account_state
-            .insert(transaction.account_id(), account_update.final_state_hash());
         // TODO: update notes and nullifiers.
+        let tx_id = transaction.id();
+        self.account_state
+            .insert(transaction.account_id(), (account_update.final_state_hash(), tx_id));
+
+        // Update parents to point back to this new transaction.
+        for parent in &parents {
+            // State information is currently not updated as transactions get removed from the pool.
+            // So its possible to not have the parent exist right now.
+            //
+            // TODO: consider whether we can expect this instead.
+            if let Some(parent) = self.tx_pool.get_mut(parent) {
+                parent.add_child(tx_id);
+            }
+        }
 
         // Insert transaction into pool and possibly as a root transaction.
-        let tx_id = transaction.id();
-        self.tx_pool.insert(tx_id, Transaction::new(transaction));
-        self.check_rootable(tx_id);
-
-        // TODO: update depedency graph aka add parents, and update parents.
+        self.tx_pool.insert(tx_id, Transaction::new(transaction, parents));
+        self.try_root_transaction(tx_id);
 
         Ok(())
     }
@@ -188,6 +280,11 @@ impl TransactionPool {
 pub enum AddTransactionError {
     #[error("Transaction's initial account state {expected} did not match the current account state {current}.")]
     InvalidAccountState { current: Digest, expected: Digest },
+    #[error("Transaction input data is stale. Required data fresher than {stale_limit} but inputs are from {input_block}.")]
+    StaleInputs {
+        input_block: BlockNumber,
+        stale_limit: BlockNumber,
+    },
 }
 
 struct Transaction {
@@ -198,12 +295,12 @@ struct Transaction {
 }
 
 impl Transaction {
-    /// Creates a new in-queue transaction with no children and no parents.
-    fn new(data: ProvenTransaction) -> Self {
+    /// Creates a new in-queue transaction with no children.
+    fn new(data: ProvenTransaction, parents: BTreeSet<TransactionId>) -> Self {
         Self {
             data: Arc::new(data),
             status: TransactionStatus::InQueue,
-            parents: Default::default(),
+            parents,
             children: Default::default(),
         }
     }
