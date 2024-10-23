aad6cacc04354bf9313d29f4a314f7933d07a4fc
Ignacio Amigo <ignacio.amigo@lambdaclass.com>
2024-10-23 10:03:37 -0300

    Proto rebuild

--- crates/proto/src/generated/responses.rs
+++ crates/proto/src/generated/responses.rs
@@ -227,7 +227,7 @@ pub struct AccountStateHeader {
     /// Values of all account storage slots (max 255).
     #[prost(bytes = "vec", tag = "2")]
     pub storage_header: ::prost::alloc::vec::Vec<u8>,
-    /// Account code, returned only in the case where the request code commitment does not match
+    /// Account code, returned only in the case where the request's code commitment does not match
     /// with the current one.
     #[prost(bytes = "vec", optional, tag = "3")]
     pub account_code: ::core::option::Option<::prost::alloc::vec::Vec<u8>>,
