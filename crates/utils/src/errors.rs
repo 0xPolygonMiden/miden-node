f91fecbfdbd088713e9f1619f9276b053abb30fa
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2025-01-13 12:59:33 +0200

    Fixup error utils

--- crates/utils/src/errors.rs
+++ crates/utils/src/errors.rs
@@ -3,22 +3,22 @@ use tonic::transport::Error as TransportError;
 
 #[derive(Debug, Error)]
 pub enum ApiError {
-    #[error("An I/O error has occurred: {0}")]
+    #[error("an I/O error has occurred")]
     IoError(#[from] std::io::Error),
 
     #[error("initialisation of the Api has failed: {0}")]
     ApiInitialisationFailed(String),
 
-    #[error("Serving the Api server has failed.")]
-    ApiServeFailed(TransportError),
+    #[error("serving the Api server has failed")]
+    ApiServeFailed(#[from] TransportError),
 
-    #[error("Resolution of the server address has failed: {0}")]
+    #[error("resolution of the server address has failed: {0}")]
     AddressResolutionFailed(String),
 
     /// Converting the provided `Endpoint` into a socket address has failed
-    #[error("Converting the `Endpoint` into a socket address failed: {0}")]
+    #[error("converting the `Endpoint` into a socket address failed: {0}")]
     EndpointToSocketFailed(std::io::Error),
 
-    #[error("Connection to the database has failed: {0}")]
+    #[error("connection to the database has failed: {0}")]
     DatabaseConnectionFailed(String),
 }
