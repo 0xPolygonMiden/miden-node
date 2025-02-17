8e21534a400cc8594d46f1aab1c3afc981830f6e
sergerad <serge.radinovich@immutable.com>
2025-02-17 20:30:58 +1300

    Add sealed and set_error impl

--- crates/utils/src/tracing/mod.rs
+++ crates/utils/src/tracing/mod.rs
@@ -1,20 +1,27 @@
 pub mod grpc;
 
-use opentelemetry::{Key, Value}; // TODO: do we want to leak these through our trait?
+use opentelemetry::trace::Status as OtelStatus;
+use opentelemetry::{Key, Value};
+use sealed::sealed;
 
-/// ...
+/// Utility functions based on [`tracing_opentelemetry::OpenTelemetrySpanExt`].
+#[sealed]
 pub trait OpenTelemetrySpanExt {
     fn set_attribute(&self, key: impl Into<Key>, value: impl Into<Value>);
     fn set_error(&self, err: &dyn std::error::Error);
 }
 
+#[sealed]
 impl OpenTelemetrySpanExt for tracing::Span {
-    /// ...
+    /// Sets an attribute on `Span`.
     fn set_attribute(&self, key: impl Into<Key>, value: impl Into<Value>) {
         tracing_opentelemetry::OpenTelemetrySpanExt::set_attribute(self, key, value);
     }
-    /// ...
-    fn set_error(&self, _err: &dyn std::error::Error) {
-        todo!();
+    /// Sets a status on `Span` based on an error.
+    fn set_error(&self, err: &dyn std::error::Error) {
+        tracing_opentelemetry::OpenTelemetrySpanExt::set_status(
+            self,
+            OtelStatus::Error { description: format!("{err:?}").into() },
+        );
     }
 }
