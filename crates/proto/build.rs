a8917d2007b3121cf0609ce61752e791d613ad6e
tomasarrachea <tomas.arrachea@lambdaclass.com>
2025-03-14 12:55:30 -0300

    review: aggregate proto builders into a single one

--- crates/proto/build.rs
+++ crates/proto/build.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use anyhow::Context;
-use miden_node_proto_build::{BlockProducerBuilder, RpcBuilder, StoreBuilder};
+use miden_node_proto_build::ProtoBuilder;
 
 /// Generates Rust protobuf bindings from .proto files in the root directory.
 ///
@@ -30,19 +30,7 @@ fn main() -> anyhow::Result<()> {
     // Build the proto files in the destination directory.
     let builder = tonic_build::configure().out_dir(&dst_dir);
 
-    #[cfg(feature = "rpc-client")]
-    let with_client = true;
-    #[cfg(not(feature = "rpc-client"))]
-    let with_client = false;
-
-    RpcBuilder::new(builder.clone())
-        .with_client(with_client)
-        .compile()
-        .context("compiling rpc.proto")?;
-    StoreBuilder::new(builder.clone()).compile().context("compiling store.proto")?;
-    BlockProducerBuilder::new(builder)
-        .compile()
-        .context("compiling block_producer.proto")?;
+    ProtoBuilder::new(builder).compile().context("compiling proto files")?;
 
     generate_mod_rs(&dst_dir).context("generating mod.rs")?;
 
--- crates/proto_build/src/lib.rs
+++ crates/proto_build/src/lib.rs
@@ -2,69 +2,42 @@ use std::{env, fs, path::PathBuf};
 
 use anyhow::Context;
 use prost::Message;
-use tonic_build::Builder;
 
-/// A builder for the `rpc` proto bindings.
-pub struct RpcBuilder(Builder);
-impl RpcBuilder {
-    /// Creates a new `RpcBuilder` from the provided `Builder`.
+pub struct ProtoBuilder(tonic_build::Builder);
+
+const RPC_PROTO: &str = "rpc.proto";
+
+#[cfg(feature = "internal")]
+const STORE_PROTO: &str = "store.proto";
+
+#[cfg(feature = "internal")]
+const BLOCK_PRODUCER_PROTO: &str = "block_producer.proto";
+
+impl ProtoBuilder {
+    /// Creates a new `Builder` from the provided `tonic_build::Builder`.
     /// The settings on the `Builder` are used to compile the proto files, including the `out_dir`
     /// directory.
-    /// By default, the client is not included in the bindings (see `RpcBuilder::with_client`).
-    pub fn new(builder: Builder) -> Self {
+    /// By default, the client is not included in the bindings (see `Builder::with_client`).
+    pub fn new(builder: tonic_build::Builder) -> Self {
         Self(builder.build_client(false))
     }
 
-    /// Sets whether the bindings should include the client.
-    #[must_use]
-    pub fn with_client(self, client: bool) -> Self {
-        Self(self.0.build_client(client))
-    }
-
-    /// Compiles the proto bindings for the `rpc` service.
+    /// Compiles the proto bindings.
     /// Generated files are written to the `out_dir` set on the internal Builder.
     pub fn compile(self) -> anyhow::Result<()> {
-        generate_protos(self.0, "rpc.proto")
+        #[cfg(feature = "internal")]
+        return generate_protos(
+            self.0.build_client(true),
+            &[RPC_PROTO, STORE_PROTO, BLOCK_PRODUCER_PROTO],
+        );
+
+        #[cfg(not(feature = "internal"))]
+        generate_protos(self.0.build_client(false), &[RPC_PROTO])
     }
 }
 
-/// A builder for the `store` proto bindings.
-pub struct StoreBuilder(Builder);
-impl StoreBuilder {
-    /// Creates a new `StoreBuilder` from the provided `Builder`.
-    /// The settings on the `Builder` are used to compile the proto files, including the `out_dir`
-    /// directory.
-    pub fn new(builder: Builder) -> Self {
-        Self(builder)
-    }
-
-    /// Compiles the proto bindings for the `store` service.
-    /// Generated files are written to the `out_dir` set on the internal Builder.
-    pub fn compile(self) -> anyhow::Result<()> {
-        generate_protos(self.0, "store.proto")
-    }
-}
-
-/// A builder for the `block_producer` proto bindings.
-pub struct BlockProducerBuilder(Builder);
-impl BlockProducerBuilder {
-    /// Creates a new `BlockProducerBuilder` from the provided `Builder`.
-    /// The settings on the `Builder` are used to compile the proto files, including the `out_dir`
-    /// directory.
-    pub fn new(builder: Builder) -> Self {
-        Self(builder)
-    }
-
-    /// Compiles the proto bindings for the `block_producer` service.
-    /// Generated files are written to the `out_dir` set on the internal Builder.
-    pub fn compile(self) -> anyhow::Result<()> {
-        generate_protos(self.0, "block_producer.proto")
-    }
-}
-
-/// This wrapper function receives a `Builder` and uses it to generate the protobuf bindings.
-/// The reason of this wrapper is to avoid the need of copying the proto definitions.
-fn generate_protos(builder: Builder, proto_file: &str) -> anyhow::Result<()> {
+/// This wrapper function receives a `tonic_build::Builder` and uses it to generate the protobuf bindings.
+fn generate_protos(builder: tonic_build::Builder, proto_files: &[&str]) -> anyhow::Result<()> {
     let cwd: PathBuf = env::current_dir().context("current directory")?;
     let cwd = cwd
         .parent()
@@ -75,10 +48,13 @@ fn generate_protos(builder: Builder, proto_file: &str) -> anyhow::Result<()> {
     let out = env::var("OUT_DIR").context("env::OUT_DIR not set")?;
     let file_descriptor_path = PathBuf::from(out).join("file_descriptor_set.bin");
 
-    let protos = &[proto_dir.join(proto_file)];
+    let protos = proto_files
+        .iter()
+        .map(|proto_file| proto_dir.join(proto_file))
+        .collect::<Vec<_>>();
 
     let includes = &[proto_dir];
-    let file_descriptors = protox::compile(protos, includes)?;
+    let file_descriptors = protox::compile(&protos, includes)?;
     fs::write(&file_descriptor_path, file_descriptors.encode_to_vec())
         .context("writing file descriptors")?;
 
@@ -87,7 +63,7 @@ fn generate_protos(builder: Builder, proto_file: &str) -> anyhow::Result<()> {
 
     builder
         .file_descriptor_set_path(file_descriptor_path)
-        .compile_protos_with_config(prost_config, protos, includes)
+        .compile_protos_with_config(prost_config, &protos, includes)
         .context("compiling protobufs")?;
     Ok(())
 }
