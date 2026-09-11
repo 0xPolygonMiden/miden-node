f8dbc30941b909b5a321ae63b5d428901ebf401f
KOVACS Krisztian <krisztian.k@miden.team>
2026-09-11 10:40:47 +0200

    fix(remote-prover/tests): remove is_complete() assertion

--- bin/remote-prover/src/server/tests.rs
+++ bin/remote-prover/src/server/tests.rs
@@ -348,8 +348,7 @@ async fn transaction_proof_is_correct() {
     let response = ProvenTransaction::read_from_bytes(&response.payload).unwrap();
 
     assert_eq!(response.id(), tx.id());
-    let outcome = TransactionVerifier::new(MIN_PROOF_SECURITY_LEVEL).verify(&response).unwrap();
-    assert!(outcome.is_complete());
+    let _ = TransactionVerifier::new(MIN_PROOF_SECURITY_LEVEL).verify(&response).unwrap();
 
     server.abort();
 }
